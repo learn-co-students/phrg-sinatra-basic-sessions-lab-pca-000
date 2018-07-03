d1d98f6dea2d26aa1298d44ce3ac6f775e7ac182
Katie Weber <k.weber0893@gmail.com>
2018-07-03 12:03:46 -0400

    Basic sessions lab

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    # binding.pry
+    @session = session
+    erb :checkout
+  end
 end
